a0ca83cd5409133bc4ce846cf7c6f8c953ec9863
Brice Carpentier <brice@daknet.org>
2017-07-23 13:25:04 +0200

    added the runner

---
+++ index.js
@@ -0,0 +1,49 @@
+const {
+  defaults,
+  debounce,
+} = require('lodash/fp');
+const nodeWatch = require('node-watch');
+
+const { series } = require('./lib/utils');
+const {
+  promisifyChildProcess,
+  runCommandWithPassTrough,
+} = require('./lib/children');
+
+const defaultOptions = {
+  restartSignal: 'SIGUSR2',
+  signals: ['SIGINT', 'SIGTERM'],
+  watch: [],
+};
+
+function runCommand(command, signals) {
+  return promisifyChildProcess(runCommandWithPassTrough(command, signals));
+}
+
+function start(commands, signals) {
+  const promises = commands.map(command => () => runCommand(command, signals));
+  return series(promises);
+}
+
+function watchAndReload(fn, files, restartSignal) {
+  return nodeWatch(files, { recursive: true }, () => {
+    process.kill(process.pid, restartSignal);
+    fn();
+  });
+}
+
+function main(commands, options = {}) {
+  const {
+    signals,
+    watch,
+    restartSignal,
+  } = defaults(defaultOptions)(options);
+  const fn = () => start(commands, restartSignal.concat(signals));
+  if (watch.length) {
+    const watcher = watchAndReload(debounce(1000)(fn), watch, restartSignal);
+    process.on('SIGINT', watcher.close);
+    process.on('SIGTERM', watcher.close);
+  }
+}
+
+module.exports = main;
